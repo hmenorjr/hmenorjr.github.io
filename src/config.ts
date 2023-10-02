68de984a6b873dadc642301ee610069ad8620502
Herman Menor <herman.menor@gmail.com>
2023-10-02 15:29:25 +0300

    Update contents and configs

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Herman Menoor, Jr | Software Engineer and Web Developer';
+export const SITE_TITLE = 'Herman Menor Jr | Software Engineer and Web Developer';
 export const SITE_DESCRIPTION = 'Herman\'s Personal Portfolio Website built with Blog, CV, Project Section, Store and RSS Feed.';
