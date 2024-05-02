34f25eb691d118d8c6a5d3ee448539c7fcf59dfd
Herman Menor <herman.menor@gmail.com>
2024-05-02 07:43:33 +0000

    Meta and welcome page updates

--- src/config.mjs
+++ src/config.mjs
@@ -1,7 +1,7 @@
 export default {
     title: "HMenorJr",
-    titleSuffix: " Web",
-    description: "HMenorJr is a website development, design and blogging for solving technical problem encounters of Herman Menor, Jr.",
+    titleSuffix: " Development and Design",
+    description: "A website development, design and blogging for solving technical problem encounters of Herman Menor, Jr.",
     defaultOgImage: "./defaultog.jpg",
     latestPosts: 3,
     postsPerPage: 3,
